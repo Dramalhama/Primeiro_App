8f63188b25120f1b35f8e682669dd8b90785e4c0
Pedro Francisco Borges De Jesus <00001124830935sp@aluno.educacao.sp.gov.br>
2026-03-09 20:54:44 -0300

    Adicionando projeto

---
+++ Primeiro_APP/Primeiro_APP/Controllers/AlunoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Primeiro_APP.Controllers
+{
+    public class AlunoController : Controller
+    {
+        [ViewData] public string nome { get; set; }
+
+        [ViewData] public string sobrenome { get;  set; }
+
+
+        public IActionResult Index()
+        {
+            ViewBag.nota = "Aluno nota 10";
+            ViewData["Escola"] = "Escola Pedro Fonseca";
+
+            nome = "Pepe";
+            sobrenome = "Borges";
+            
+            return View();
+        }
+
+        public IActionResult Teste()
+        {
+            TempData["MSG_EX"] = "Mensagem do TempData";
+            return RedirectToAction("Resultado");
+        }
+        public IActionResult Resultado()
+        {
+            if (TempData["MSG_EX"] != null)
+                ViewBag.Exemplo = "Alunos do 3°B";
+
+
+            return View();
+        }
+    }
+}
